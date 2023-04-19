4614ce232f35f2647ab8ab8c42e401ca8e7b7041
Ryfan AI <ryfandotnet@gmail.com>
2023-04-19 13:14:49 +0700

    WIP: Adding routes details, pages details, styling details

---
+++ src/pages/main/detail/index.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Meta from '../../../utils/meta';
+import Base from '../../../component/layout/base';
+
+export default function Detail() {
+ return (
+  <Fragment>
+   <Meta title="Detail" />
+   <Base>
+    <p>Detail</p>
+   </Base>
+  </Fragment>
+ );
+}
